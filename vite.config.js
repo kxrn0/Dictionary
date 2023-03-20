7ba781973daf4ae199dfd5c2f65dc8a6cac5ded7
kxrn0 <kxrp@protonmail.com>
2023-03-20 11:30:29 -0500

    I think I'm done

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import viteSvgr from 'vite-plugin-svgr'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteSvgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteSvgr()],
-})
+  base: "/Dictionary/",
+  plugins: [
+    react(),
+    viteSvgr(),
+    VitePWA({
+      workbox: { globPatterns: ["**/*"] },
+      includeAssets: ["**/*"],
+      manifest: {
+        theme_color: "#f69435",
+        background_color: "#f69435",
+        display: "standalone",
+        scope: "/Dictionary",
+        start_url: "/Dictionary",
+        short_name: "dick",
+        description: "Dictionary app",
+        name: "Dictionary",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
